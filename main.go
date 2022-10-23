da32066f8b402226b50fab75c929225cf8aa4dfe
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-23 13:58:27 -0400

    go channel server.

--- main.go
+++ main.go
@@ -2,53 +2,75 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
+
 )
+type Message struct {
+	from 	string
+	payloads string
+}
+type Demon struct{
+	messageCh chan Message
+}
 
-func main(){
-	// now := time.Now()
-	userId := 10
-	responseCh := make(chan string)
-
-	// group that waits
-	wg := &sync.WaitGroup{}
-	go fetchdata(userId, responseCh,wg) 
-	wg.Add(1)
-
-	go fUR(userId, responseCh, wg)
-	wg.Add(1)
-
-	go fUL(userId,responseCh,wg)
-	wg.Add(1)
-
-
-
-	wg.Add(1)
-	wg.Wait()
-	close(responseCh)
-
-
-
-	// fmt.Println(userData)
-	// fmt.Println(userRex)
-	// fmt.Println(userLike)
-	for reps := range responseCh {
-		fmt.Println(reps)
+func (d *Demon) demonStopandListen() (){
+	fmt.Println("sending message")
+	for {
+		msg := <- d.messageCh
+		fmt.Printf("recienced message frim %s payload %s", msg.from, msg.payloads)
 	}
 }
+func DemonServer(){
 
-func fetchdata (userId int , resresponseCh chan string, wg *sync.WaitGroup) {
-	time.Sleep(80 * time.Millisecond)
-	resresponseCh <- "user data"
+} 
+func main (){
+s := &Demon{
+	messageCh: make(chan Message),
+}
+go s.demonStopandListen()
+
 }
 
-func fUR (userid int, resresponseCh chan string,wg *sync.WaitGroup){
-	time.Sleep(120 * time.Millisecond)
-	resresponseCh <- "user rec"
-}
-func fUL (userid int, resresponseCh chan string,wg *sync.WaitGroup){
-	time.Sleep(50 * time.Millisecond)
-	resresponseCh <- "user likes"
+// func main(){
+// 	// now := time.Now()
+// 	userId := 10
+// 	// without only one at a time
+// 	// with buffer (3)
+// 	responseCh := make(chan string, 3)
+
+// 	// group that waits
+// 	wg := &sync.WaitGroup{}
+// 	go fetchdata(userId, responseCh,wg) 
+// 	wg.Add(1)
+
+// 	go fUR(userId, responseCh, wg)
+// 	wg.Add(1)
+
+// 	go fUL(userId,responseCh,wg)
+// 	wg.Add(1)
+
+// 	wg.Wait()
+// 	close(responseCh)
+
+// 	for reps := range responseCh {
+// 		fmt.Println(reps)
+// 	}
+// }
+
+// func fetchdata (userId int , resresponseCh chan string, wg *sync.WaitGroup) {
+// 	time.Sleep(80 * time.Millisecond)
+// 	resresponseCh <- "user data"
+// 	wg.Done()
+// }
+
+// func fUR (userid int, resresponseCh chan string,wg *sync.WaitGroup){
+// 	time.Sleep(120 * time.Millisecond)
+// 	resresponseCh <- "user rec"
+// 	wg.Done()
+
+// }
+// func fUL (userid int, resresponseCh chan string,wg *sync.WaitGroup){
+// 	time.Sleep(50 * time.Millisecond)
+// 	resresponseCh <- "user likes"
+// 	wg.Done()
 	 
-}
+// }
