626c0be99e0069aaffe5d26fd08dae58f03831f2
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-23 14:56:14 -0400

    added test for clarification.

--- race_test.go
+++ race_test.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-	"sync"
+
+	"sync/atomic"
 	"testing"
 )
 
 func testDataRaceCondition(l *testing.T) {
 	var state int32
-	var mu sync.RWMutex
+	// state = 1
+	// s+1
+	// s+1
+	// var mu sync.RWMutex
 
 	// go x  takes state manipulate
 	// go y  same thing 
 		// both modify state
 		for i := 0; i < 10; i++ {
 			go func (i int)  {
-				// hold acces to the var of the mu
-				mu.Lock()
+				// add state to the already existing value  
+				atomic.AddInt32(&state, int32(i))
+
+					// hold acces to the var of the mu
+				// mu.Lock()
 				state += int32(i)
-				// base logic
-				mu.Unlock()
+					// base logic
+				// mu.Unlock()
 			}(i)
 		}
 }
