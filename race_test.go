0ffb6ae08e2600ebe89388bcbfd4748c0e2cb269
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-23 14:34:51 -0400

    mutex logic and syntax.

--- race_test.go
+++ race_test.go
@@ -1,12 +1,24 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func testDataRaceCondition(l *testing.T) {
 	var state int32
+	var mu sync.RWMutex
 
 	// go x  takes state manipulate
 	// go y  same thing 
 		// both modify state
-	
+		for i := 0; i < 10; i++ {
+			go func (i int)  {
+				// hold acces to the var of the mu
+				mu.Lock()
+				state += int32(i)
+				// base logic
+				mu.Unlock()
+			}(i)
+		}
 }
